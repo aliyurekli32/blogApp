3d9e387457bb4d1e450bf5ecd2e09ee1faf838f0
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-21 09:46:57 +0300

    update: settings(secret key)

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(+=w%yup@#v!o&9o99rs2b5ox-%ja2^u3o=pr59fhm*t=bvrke'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

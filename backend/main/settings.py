95e05a81a7cfc7805c29f9ac1420647f0f898d8d
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-21 10:08:03 +0300

    update: extra apps.

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # extra-apps
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [

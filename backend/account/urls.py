877e6561b1764a49d0d5f214fa542ad371b80752
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-21 21:00:41 +0300

    update: update section

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -3,11 +3,13 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import RegisterView
+from .views import RegisterView,ChangePasswordView,UpdateProfileView
 
 urlpatterns = [
     # path('/', include('rest_framework.urls')),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
+    path('change_password/<int:pk>/', ChangePasswordView.as_view(), name='auth_change_password'),
+    path('update_profile/<int:pk>/', UpdateProfileView.as_view(), name='auth_update_profile'),
 ]
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 from rest_framework.permissions import AllowAny
 from .models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer,ChangePasswordSerializer,UpdateUserSerializer
 from rest_framework import generics
 
 
@@ -11,3 +11,18 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+    
+
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
+    
+class UpdateProfileView(generics.UpdateAPIView):
+
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UpdateUserSerializer

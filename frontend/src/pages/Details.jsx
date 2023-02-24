36c25adc34b5918ffdefd7e984a7073b0ea0da76
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-24 11:56:19 +0300

    update: cards

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
-const Details = () => {
+const Details = (props) => {
+  const {post}=props
   return (
-    <div>Details</div>
+    <>
+      <div className="col-lg-4 col-md-6 mb-4">
+        <div className="card">
+          <div className="view overlay">
+            <img
+              className="card-img-top"
+              src={post?.image_url}
+              alt={post?.title}
+            />
+            <a href="#!">
+              <div className="mask rgba-white-slight"></div>
+            </a>
+          </div>
+          <div className="card-body">
+            <h4 className="card-title">{post?.title}</h4>
+            <p className="card-text">{post?.summary}</p>
+            <a href={`/blog/${post?.id}`} className="btn btn-primary">
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 

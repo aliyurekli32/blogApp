43952d229e470c37459ec444b6ae2df46d7cfcb7
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-24 10:28:50 +0300

    add: api

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Category, Blog, Comment, Likes, PostViews
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Blog)
+admin.site.register(Comment)
+admin.site.register(Likes)
+admin.site.register(PostViews)

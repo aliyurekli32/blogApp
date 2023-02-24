b55030aed260b066e0f85ca0420c5c230bc21dfe
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-24 10:29:53 +0300

    add: api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from .views import BlogView, CategoryView, CommentView, LikesView, PostViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('categories', CategoryView)
+router.register('blogs', BlogView)
+router.register('comments', CommentView)
+router.register('likes', LikesView)
+router.register('post_views', PostViewSet)
 
 urlpatterns = [
     
-]
+] + router.urls

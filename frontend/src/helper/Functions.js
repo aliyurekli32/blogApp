2dfabe8db13912c0baedead9068d189bac59fdec
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-24 11:55:59 +0300

    update: cards

--- frontend/src/helper/Functions.js
+++ frontend/src/helper/Functions.js
@@ -81,4 +81,26 @@ export const fetchUser=async(a)=>{
     
 }
 
+export const passUpdate=async(data)=>{
+    try {
+        await fetch(`http://127.0.0.1:8000/auth/change_password/${data?.id}/`,
+        {
+            method: "PUT",
+            body: JSON.stringify({
+                "old_password": data.old_password,
+                "password": data.password,
+                "password2": data.password2
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                "Authorization": `Bearer ${data.access}`
+            }
+
+        }
+        ).then(res=>res.json()).then(data=>console.log(data))
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
 

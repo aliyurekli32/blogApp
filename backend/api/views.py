f60bda90bdc0adb94df38debb07f4dde44e1baaa
aliyurekli32 <adamjesperblack@gmail.com>
2023-03-02 23:48:16 +0300

    update:

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,72 +1,72 @@
 from django.shortcuts import render
+
 from .models import Category, Blog, Comment, Likes, PostViews
 from .serializers import CategorySerializer, BlogSerializer, CommentSerializer, LikesSerializer, PostViewsSerializer, UserBlogSerializer
-from rest_framework.viewsets import ModelViewSet
 from .permissions import IsStaffOrReadOnly, IsOwnerOrReadOnly, IsOwnerOrReadOnlyComment
+
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
 
 class CategoryView(ModelViewSet):
-    
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsStaffOrReadOnly]  # category işlemleriniden sadece get işlemini her login olan kullanıcı görebilir ama put post delete işlemlerini sadece staff user yapabilir.
-     
+    #! category işlemlerininde sadece get işlemini her kullanıcı yapabilir, put post delete işlemlerini sadece staff user yapabilir:
+    #! # Any user can only get category operations, only staff user can put post delete operations:
+    permission_classes = [IsStaffOrReadOnly]  
 
 class BlogView(ModelViewSet):
-    
     queryset = Blog.objects.all()
     serializer_class = UserBlogSerializer
-    permission_classes = [IsOwnerOrReadOnly]  # Bloglarda get işlemini login olan her kullanıcı yapabilecek update ve delete işlemlerini yalnızca blog sahibi yapabilecek
+    #! Bloglarda get işlemini her kullanıcı yapabilecek update ve delete işlemlerini yalnızca blog sahibi ve staff user yapabilecek:
+    #! Only the blog owner and staff user can perform the update and delete operations on the blogs:
+    permission_classes = [IsOwnerOrReadOnly]  
     
     def perform_create(self, serializer):
        serializer.save(author=serializer.context['request'].user)
-    # def create(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_create(serializer)
-    #     headers = self.get_success_headers(serializer.data)
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+    #! Blog sayfasının detayına girdiğimizde post_view create edilecek:
+    #! When we enter the details of the blog page, the post_view will be created:
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        view_query = PostViews.objects.filter(user = request.user, post = instance)
+        if not view_query.exists():  # daha önce detayı inceleyen kullanıcı yok ise create işlemi yapılacak #? If there is no user who has examined the detail before, the create operation will be done
+            PostViews.objects.create(user = request.user, post = instance, post_views = True)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
-    # def perform_create(self, serializer):
-    #     serializer.save()
-
+    #! Kullanıcı eğer blog sahibi ya da staff ise bloglarının status'u 'd' olsa bile görüntüleyip düzenleyebilecek:
+    #! If the user is the owner or team of the blog, they can view and edit it, even if the blog status is 'd':
     def get_queryset(self):
-        
         if self.request.user.is_staff:
            return super().get_queryset()
         if super().get_queryset().filter(author = self.request.user.id) :
             return super().get_queryset()
         else:
-            return super().get_queryset().filter(status='p')  # sadece yayın statusu public olan blogları yayınla
+            return super().get_queryset().filter(status='p')  # sadece yayın statusu public olan blogları yayınla #? only publish blogs with public post status
 
+    #! Kullanı staff ise BlogSerializer kullansın değil ise UserBlogSerializer'ı kullansın:
+    #! If the user is staff, let him use the Blog Serializer; otherwise, let him use the UserBlogSerializer:
     def get_serializer_class(self):
         serializer = super().get_serializer_class()
         if self.request.user.is_staff:
             return BlogSerializer
         return serializer
-    
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # # TEK KULLAN
-    
-    # def retrieve(self, request, *args, **kwargs):
-    #     instance = self.get_object()
-    #     if PostViews.objects.get(user_id=request.user.id) and  PostViews.objects.get(post_id = instance.id):
-    #         PostViews.objects.get(post_views = True)
-    #     serializer = self.get_serializer(instance)
-    #     return Response(serializer.data)
 
 
 class CommentView(ModelViewSet):
     
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsOwnerOrReadOnlyComment]  # her kullanıcı kendi yorumunu düzenleyebilecek herkesin yorumunu görüntüleyebilecek
+    #! Her kullanıcı kendi yorumunu düzenleyebilecek herkesin yorumunu görüntüleyebilecek:
+    #! Each user will be able to view anyone's comment who can edit their own comment:
+    permission_classes = [IsOwnerOrReadOnlyComment]  
     def perform_create(self, serializer):
        serializer.save(user=serializer.context['request'].user)
+
+
 class LikesView(ModelViewSet):
     queryset = Likes.objects.filter(likes = True)
     serializer_class = LikesSerializer
@@ -85,12 +85,9 @@ class LikesView(ModelViewSet):
     def perform_destroy(self, instance):
         instance.delete()
        
-    # def get_queryset(self):
-    #     if Likes.objects.filter(likes = False):
-    #        obj = Likes.objects.filter(likes = False).delete()
-    #        obj.save()
-    #     return obj
-    
 class PostViewSet(ModelViewSet):
     queryset = PostViews.objects.all()
     serializer_class = PostViewsSerializer
+    
+    def perform_create(self, serializer):
+       serializer.save(user=serializer.context['request'].user)

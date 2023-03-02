0c907e3cbf91e2a288f1516fb8b371dc02e4b570
aliyurekli32 <adamjesperblack@gmail.com>
2023-03-02 23:46:01 +0300

    update:

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -3,16 +3,17 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import RegisterView,ChangePasswordView,UpdateProfileView,LogoutView,LogoutAllView,ProfileUser,CustomTokenObtainPairView
+from .views import RegisterView,ChangePasswordView,UpdateProfileView,LogoutView,LogoutAllView,loginView,CookieTokenRefreshView,ProfileUser,ResetView
 
 urlpatterns = [
     # path('/', include('rest_framework.urls')),
     path('user/', ProfileUser.as_view()),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', loginView, name='token_obtain_pair'),
+    path('login/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('change_password/<int:pk>/', ChangePasswordView.as_view(), name='auth_change_password'),
     path('update_profile/<int:pk>/', UpdateProfileView.as_view(), name='auth_update_profile'),
     path('logout/', LogoutView.as_view(), name='auth_logout'),
+    path('reset/', ResetView.as_view(), name='auth_reset'),
     path('logout_all/', LogoutAllView.as_view(), name='auth_logout_all'),
 ]
